85d2d0a638d8f2a479bae0029a16feefc0a95607
AS <arunsalgia@gmail.com>
2021-06-02 09:42:32 +0530

    react match details every hour

--- app.js
+++ app.js
@@ -461,7 +461,7 @@ GROUP1_MAXBALANCE = 1000;
 allUSER = 99999999;
 
 // Number of hours after which match details to be read frpom cricapi.
-MATCHREADINTERVAL = 3;
+MATCHREADINTERVAL = 1;
 
 // Wallet 
 // WalletStatus = {success: "success", failed: "success"};
